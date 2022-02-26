caaf3e40010bb4e0fc9454509b892d7b842e9948
Keith Stevens <fozziethebeat@gmail.com>
2022-02-26 17:05:57 +0900

    Adding tables for language info and blending it into the language stats results

--- web/pages/api/language_stats.ts
+++ web/pages/api/language_stats.ts
@@ -6,10 +6,21 @@ import type { Language } from '@features/language';
 
 const prisma = new PrismaClient();
 
+/**
+ * Returns an array of LanguageStats given a requested list of languages and
+ * tasks for those languages.
+ *
+ * This requires calls to multiple database tables:
+ *   - LanguageDetails
+ *   - LanguageFunFacts
+ *   - TaskMilestones
+ */
 export default async (
   req: NextApiRequest,
   res: NextApiResponse<Array<LanguageStats>>
 ) => {
+  // Get the languages and factor that into a set of langauge strings and a
+  // mapping from languages to task IDs.
   const languagesAndTasks = req.body as Language[];
   const languages = languagesAndTasks.map(({language}) => language);
   const requestedTasks = languagesAndTasks.reduce( (results, entry) => {
@@ -17,17 +28,55 @@ export default async (
     return results;
   }, new Map);
 
+  // Fetch language details for each language and form it into a Map.
+  let languageDetails = await prisma.languageDetails.findMany({
+        where: {
+          language: { in: languages },
+        }
+      });
+  languageDetails = languageDetails.reduce( (result, entry) => {
+        result.set(entry.language, entry.description);
+        return result;
+   }, new Map);
+
+   // Fetch language fun facts for each language and form it into a Map.
+   let languageFunFacts = await prisma.languageFunFacts.findMany({
+        where: {
+          language: { in: languages },
+        }
+      });
+  languageFunFacts = languageFunFacts.reduce( (result, entry) => {
+        if (result.has(entry.language)) {
+          result.get(entry.language).push(entry.fact);
+        } else {
+          result.set(entry.language, [entry.fact]);
+        }
+        return result;
+      }, new Map);
+
+
+  // Get all task stats for the requested languages.
   const taskStats = await prisma.taskMilestones.findMany({
         where: {
           language: { in: languages },
           milestoneType: "weekly",
         }
       });
+  
+  // Given the results above, convert everything into a list of LanguageStats.
+  // 
+  // We do this in a multi-step process:
+  //   1) Compress the array into a Map from languages to an array of
+  //   TaskStats.
+  //   2) Convert the Map's entries to LanguageStats and return that as an
+  //   Array.  During the conversion, add in the language info.
   const languageStats = Array.from(
     taskStats.reduce( (results, stats) => {
       const taskStats = results.has(stats.language) ?
         results.get(stats.language) : [];
 
+      // Only add the TaskStats if the user requested the task for the
+      // language.
       const taskSet = requestedTasks.get(stats.language);
       if (taskSet.has(stats.taskId)) {
         taskStats.push({
@@ -43,15 +92,17 @@ export default async (
     new Map
     ).entries())
     .map( ([key, value]) => {
+      // Map to a LanguageStat object.
       return {
         language: key,
         taskStats: value,
         info: {
-          description: 'blah',
-          funFact: 'gar',
+          description: languageDetails.get(key),
+          funFact: languageFunFacts.get(key)[0],
         }
       } as LanguageStats;
     });
 
+  // Done~
   res.status(200).json(languageStats);
 }
