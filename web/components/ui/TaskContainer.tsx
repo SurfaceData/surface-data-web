0087c737fc6120c6dbf5d92cb10ff25549757902
Keith Stevens <fozziethebeat@gmail.com>
2022-04-07 11:49:45 +0900

    Adding the missing visual components

---
+++ web/components/ui/TaskContainer.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const TaskContainer = styled.div`
+  display: flex;
+  gap: 12px;
+  flex-direction: column;
+`;
---
+++ web/components/ui/TaskContentContainer.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const TaskContentContainer = styled.div`
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 12px;
+`;
