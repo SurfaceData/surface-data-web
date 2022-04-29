856fe92dc7a14674ca5760636e52b374ab9840ae
Keith Stevens <fozziethebeat@gmail.com>
2022-04-29 13:51:33 +0900

    Making profile nagivation and logout a bit cleaner

--- web/components/Header.tsx
+++ web/components/Header.tsx
@@ -1,11 +1,14 @@
-import { Trans } from '@lingui/macro';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useSession, signIn, signOut } from 'next-auth/react';
-import styled from 'styled-components';
+import { Trans } from "@lingui/macro";
+import md5 from "md5";
+import Image from "next/image";
+import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { Dropdown } from "rsuite";
+import styled from "styled-components";
 
-import { Button } from '@components/ui/Button';
-import { LightBlue } from '@styles/palettes';
+import { NextLink } from "@components/NextLink";
+import { Button } from "@components/ui/Button";
+import { LightBlue } from "@styles/palettes";
 
 const StyledNav = styled.nav`
   align-items: center;
@@ -27,22 +30,32 @@ const StyledProfile = styled.img`
   border-radius: 50%;
 `;
 
+const ProfileButton = styled.button`
+  background-color: transparent;
+  border-radius: 50%;
+`;
+
+function getGravatarURL(email) {
+  const address = String(email).trim().toLowerCase();
+  const hash = md5(address);
+  return `https://www.gravatar.com/avatar/${hash}`;
+}
+
+const renderProfileIcon = (props, ref) => {
+  const { user, ...rest } = props;
+  const image = user.image ? user.image : getGravatarURL(user.email);
+  return (
+    <ProfileButton {...rest} ref={ref}>
+      <StyledProfile alt="Your Profile" src={image} height="32" width="32" />
+    </ProfileButton>
+  );
+};
+
 const Header = () => {
   const { data: session } = useSession();
   if (session) {
     return (
       <StyledNav>
-        <Link href="/profile" passHref>
-          <a>
-            <StyledProfile
-                alt="Your Profile"
-                src={session.user.image}
-                height="32"
-                width="32"
-            />
-          </a>
-        </Link>
-
         <Logo>
           <Link href="/">
             <a>
@@ -50,43 +63,52 @@ const Header = () => {
                 alt="Surface Data"
                 src="/surface.svg"
                 height="32"
-                width="32" />
+                width="32"
+              />
             </a>
           </Link>
-          <span>| <Trans id='header-sdc'>Surface Data Collective</Trans></span>
+          <span>
+            | <Trans id="header-sdc">Surface Data Collective</Trans>
+          </span>
         </Logo>
 
-        <Button
-          rounded
-          outline
-          onClick={() => signOut({
-            callbackUrl: `${window.location.origin}`,
-          })}>
-          Signout 
-        </Button>
+        <Dropdown
+          placement="bottomEnd"
+          title="Account"
+          renderToggle={renderProfileIcon}
+          user={session.user}
+        >
+          <Dropdown.Item style={{ width: 150 }} as={NextLink} href="/profile">
+            Profile
+          </Dropdown.Item>
+          <Dropdown.Item
+            style={{ width: 150 }}
+            onSelect={() =>
+              signOut({
+                callbackUrl: `${window.location.origin}`,
+              })
+            }
+          >
+            Logout
+          </Dropdown.Item>
+        </Dropdown>
       </StyledNav>
-    )
+    );
   }
   return (
     <StyledNav>
       <Logo>
-        <Image
-          alt="Surface Data"
-          src="/surface.svg"
-          height="32"
-          width="32"
-        />
-        <span>| <Trans id='header-sdc'>Surface Data Collective</Trans></span>
+        <Image alt="Surface Data" src="/surface.svg" height="32" width="32" />
+        <span>
+          | <Trans id="header-sdc">Surface Data Collective</Trans>
+        </span>
       </Logo>
 
-      <Button
-        rounded
-        palette={LightBlue}
-        onClick={() => signIn()}>
+      <Button rounded palette={LightBlue} onClick={() => signIn()}>
         Login / Register
       </Button>
     </StyledNav>
-  )
+  );
 };
 
 export default Header;
---
+++ web/components/NextLink.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Link from 'next/link';
+
+export const NextLink = React.forwardRef((props, ref) => {
+  const { href, as, ...rest } = props;
+  return (
+    <Link href={href} as={as}>
+      <a ref={ref} {...rest} />
+    </Link>
+  );
+});
